1df63a83382f67208e916121901f8bdf7f64b3e5
JinYu <1137863747@qq.com>
2020-10-21 15:33:47 +0800

    Ribbon负载均衡使用LoadBalancerClient，添加子模块ribbon-client

--- eureka-ribbon-client/src/main/java/com/example/eurekaribbonclient/Controller/RibbonController.java
+++ eureka-ribbon-client/src/main/java/com/example/eurekaribbonclient/Controller/RibbonController.java
@@ -2,6 +2,8 @@ package com.example.eurekaribbonclient.Controller;
 
 import com.example.eurekaribbonclient.Service.RibbonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,10 +14,17 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/ribbon")
 public class RibbonController {
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
     @Resource
     private RibbonService ribbonService;
     @GetMapping("/hi")
     public String hi(@RequestParam(required = false,defaultValue = "JinYu") String name){
         return ribbonService.hi(name);
+    }
+    @GetMapping("/hi1")
+    public String hi1(@RequestParam(required = false,defaultValue = "JinYu") String name){
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        return serviceInstance.getHost() + "c"+name+":" + serviceInstance.getPort();
     }
 }
